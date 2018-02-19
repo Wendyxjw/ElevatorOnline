912481ceb565a7a8d881494ed07aa6b0bc9f5e9c
xjw <390589142@qq.com>
2018-02-19 20:09:14 +0800

    弹窗静态

--- src/views/index/left3.vue
+++ src/views/index/left3.vue
@@ -11,11 +11,11 @@
     color: #fff;
     margin-right: 5px;
   }
-  .line{
+  .line {
     display: inline-block;
     flex: 1;
   }
-  .line-percent{
+  .line-percent {
     height: 8px;
     display: inline-block;
     position: relative;
@@ -23,13 +23,27 @@
     left: 0;
     border-radius: 10px;
   }
-  .line-yellow{
-    background-color: #CCCC00;
+  .line-yellow {
+    background-color: #cccc00;
   }
-  .line-blue{
+  .line-blue {
     background-color: @blue;
   }
-
+}
+.leftmodal {
+  .mtitle {
+    font-size: 16px;
+    text-align: center;
+    line-height: 40px;
+    font-weight: bold;
+    border-bottom: 1px solid #dddddd;
+    margin-bottom: 10px;
+  }
+  .chart{
+    width: 580px;
+    height: 400px;
+    padding: 10px;
+  }
 }
 </style>
 <template>
@@ -46,7 +60,7 @@
         <p class="text-middle-white">维保完成情况</p>
         <div class="text-center text-large-blue">5000次</div>
       </div>
-      <div>
+      <div  @click="leftmodalShow">
         <p class="text-middle-white">维保完成率</p>
         <div class="text-center ">
           <Circle :percent="40" :size="50" :stroke-width="13" :trail-width="13">
@@ -80,15 +94,190 @@
 
    
     </div>
+    <Modal
+        v-model="leftmodal1"
+        width="1000"
+        class-name="leftmodal">
+        <header class="mtitle">
+          本月维保完成率
+        </header>
+        <Row>
+          <Col span="10">
+          <Table :columns="columns1" :data="data1"></Table>
+          </Col>
+          <Col span="14">
+          <div id="left3chart1" class="chart"></div>
+          </Col>
+        </Row>
+
+    </Modal>
   </div>
 </template>
 <script>
+var echarts = require("echarts");
+
 export default {
   name: "left3",
   data() {
     return {
-      title: "维保情况"
+      title: "维保情况",
+      leftmodal1: false,
+      columns1: [
+        {
+          title: "区域",
+          key: "name"
+        },
+        {
+          title: "计划",
+          key: "age"
+        },
+        {
+          title: "实际",
+          key: "address"
+        },
+        {
+          title: "完成率",
+          key: "date"
+        }
+      ],
+      data1: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03"
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01"
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02"
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04"
+        }
+      ]
     };
+  },
+  methods: {
+    leftmodalShow() {
+      this.leftmodal1 = true;
+      this.initchart();
+    },
+    initchart() {
+      var myChart = echarts.init(document.getElementById("left3chart1"));
+      var option = {
+        title:{
+          text:"图表标题",
+           x: "center",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            crossStyle: {
+              color: "#999"
+            }
+          }
+        },
+        toolbox: {
+          // feature: {
+          //   dataView: { show: true, readOnly: false },
+          //   magicType: { show: true, type: ["line", "bar"] },
+          //   restore: { show: true },
+          //   saveAsImage: { show: true }
+          // }
+        },
+        legend: {
+          data: ["蒸发量", "降水量", "平均温度"],
+          bottom:0
+        },
+        xAxis: [
+          {
+            type: "category",
+            data: [
+              "华北",
+              "华东",
+              "华南",
+              "东北",
+              "西南西北"
+            ],
+            axisPointer: {
+              type: "shadow"
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: "value",
+            name: "",
+            min: 0,
+            max: 250,
+            interval: 50,
+            axisLabel: {
+              formatter: "{value} "
+            }
+          },
+          {
+            type: "value",
+            name: "",
+            min: 0,
+            max: 25,
+            interval: 5,
+            axisLabel: {
+              formatter: "{value} %"
+            }
+          }
+        ],
+        series: [
+          {
+            name: "计划",
+            type: "bar",
+            data: [
+              2.0,
+              4.9,
+              7.0,
+              23.2,
+              25.6,
+            ]
+          },
+          {
+            name: "实际",
+            type: "bar",
+            data: [
+              2.6,
+              5.9,
+              9.0,
+              26.4,
+              28.7
+            ]
+          },
+          {
+             name: "完成率",
+            type: "line",
+            yAxisIndex: 1,
+            data: [
+              2.0,
+              2.2,
+              3.3,
+              4.5,
+              6.3
+            ]
+          }
+        ]
+      };
+      // 绘制图表
+      myChart.setOption(option);
+    }
   }
 };
 </script>
